d5f8777bceb58b85ecb1cf99d1a925501681e2e5
Vitaliy Karakushan <karakushan@gmail.com>
2018-01-13 14:05:21 +0200

    добавил стилизацию вкладок языков в админке

--- templates/field/file.php
+++ templates/field/file.php
@@ -1,15 +1,15 @@
 <?php
 if ( empty( $value ) ||  !is_numeric($value) ) {
-	$src = '<img src="' . ES_DIR_URL . '"img/no-image.png">';
+	$src = '<img src="'.esc_url(ES_NO_IMAGE_URL).'">';
 } else {
-	$src = wp_get_attachment_link( $value, 'thumbnail', false, true, null );
+	$src = wp_get_attachment_image($value,'thumbnail',true);
 }
 
 ?>
 <div class="file"><?php echo $src; ?></div>
 <div>
     <input type="hidden" name="easy[<?php echo $name ?>]" value="<?php echo $value ?>"/>
-    <button type="button" class="upload_image_button button"
+    <button type="button" class="upload_image_button button button-primary"
             data-type="file"><?php _e( 'Add', 'easy-start' ) ?></button>
     <button type="button" class="remove_image_button button">&times;</button>
 </div>
