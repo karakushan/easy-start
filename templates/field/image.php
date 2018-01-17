740a4398d5c034357858f417e7b45c8ab8d3b371
Vitaliy Karakushan <karakushan@gmail.com>
2018-01-17 22:09:56 +0200

    реализовал отображать фото не только по id atachment

--- templates/field/image.php
+++ templates/field/image.php
@@ -1,6 +1,8 @@
 <?php
-if ( empty( $value ) && ! is_numeric( $value ) ) {
+if ( empty( $value ) ) {
 	$src = '<img src="' . esc_url( ES_NO_IMAGE_URL ) . '">';
+} elseif ( ! is_numeric( $value ) ) {
+	$src = '<img src="' . esc_url( $value ) . '">';
 } else {
 	$src = '<img src="' . esc_url( wp_get_attachment_image_url( $value, 'thumbnail' ) ) . '">';
 }
@@ -9,9 +11,12 @@ if ( empty( $value ) && ! is_numeric( $value ) ) {
 <div class="file"><?php echo $src; ?></div>
 <div>
   <input type="hidden" name="easy[<?php echo $name ?>]" value="<?php echo $value ?>"/>
-  <button type="button" class="upload_image_button button button-primary" data-action="select-image" title="добавить изображение"
+  <button type="button" class="upload_image_button button button-primary" data-action="select-image"
+          title="добавить изображение"
           style="width: 126px;">
 	  <?php _e( 'Add', 'easy-start' ) ?>
   </button>
-  <button type="button" class="remove_image_button button" title="удалить изображение" data-no-image="<?php echo esc_url( ES_NO_IMAGE_URL ) ?>" data-action='remove-atachment'>&times;</button>
+  <button type="button" class="remove_image_button button" title="удалить изображение"
+          data-no-image="<?php echo esc_url( ES_NO_IMAGE_URL ) ?>" data-action='remove-atachment'>&times;
+  </button>
 </div>
